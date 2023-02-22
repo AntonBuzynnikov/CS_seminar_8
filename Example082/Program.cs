c8c2bce39c1364f37ad2816e5d34d5f13a0ff5e6
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-23 01:51:49 +0700

    Complete Example082

---
+++ Example082/Program.cs
@@ -0,0 +1,10 @@
+﻿// 82.C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+int k = 0;
+foreach(char c in str)
+{
+    if(c != '.') k++;
+    else break;
+}
+System.Console.WriteLine($"Количество символов до точки: {k}");
